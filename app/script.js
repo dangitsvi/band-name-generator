0b792f6ba1781d0ed6776145f755f1d3c27dd178
dangitsvi <dangitsvi@gmail.com>
2015-06-23 01:01:15 -0700

    grunted files, not functional

--- app/script.js
+++ app/script.js
@@ -1,75 +1,87 @@
+'use strict';
+
 $(function() {
 
+  //flags for word lock feature
   var adjLocked = false;
   var verbLocked = false;
   var nounLocked = false;
 
-//jquery here
-  $("#button").on("click", function() {
-    if (!adjLocked){
-      $.get("/adjective", function(response){
+  //jquery here
+  //randomizer button
+  $('#button').on('click', function() {
+    if (!adjLocked) {
+      $.get('/adjective', function(response) {
         var adjective = response.word;
-        $("#adjective").text(adjective);
+        $('#adjective').text(adjective);
       });
     }
-    if (!verbLocked){
-      $.get("/verb", function(response){
-        var verb = " " + response.word;
-        $("#verb").text(verb);
+
+    if (!verbLocked) {
+      $.get('/verb', function(response) {
+        var verb = ' ' + response.word;
+        $('#verb').text(verb);
       });
     }
-    if (!nounLocked){
-      $.get("/noun", function(response){
-        var noun = " " + response.word;
-        $("#noun").text(noun);
+
+    if (!nounLocked) {
+      $.get('/noun', function(response) {
+        var noun = ' ' + response.word;
+        $('#noun').text(noun);
       });
     }
   });
 
-  $("#lockWords").on('submit', function(e){
+  //lock words feature
+  $('#lockWords').on('submit', function(e) {
     e.preventDefault();
 
-    var adjective = $("input[name=lockAdj]").val();
-    var verb = $("input[name=lockVerb]").val();
-    var noun = $("input[name=lockNoun]").val();
-    //this ifstatement checks to see if adjective is thruthy ie: is there a word in adjective
+    var adjective = $('input[name=lockAdj]').val();
+    var verb = $('input[name=lockVerb]').val();
+    var noun = $('input[name=lockNoun]').val();
+
     var adjPost;
     var verbPost;
     var nounPost;
 
+    //this unlocks all words so that if a form is deleted, it will unlock that word
     adjLocked = false;
     verbLocked = false;
     nounLocked = false;
 
-    if(adjective) {
+    if (adjective) {
       adjPost = {word: adjective};
-      $.post("/locked-adj", adjPost, function(response){
+      $.post('/locked-adj', adjPost, function(response) {
         var adjectiveRes = response.word;
-        $("#adjective").text(adjectiveRes);
+        $('#adjective').text(adjectiveRes);
       });
+
       adjLocked = true;
     }
 
-    if(verb) {
+    if (verb) {
       verbPost = {word: verb};
-      $.post("/locked-verb", verbPost, function(response){
+      $.post('/locked-verb', verbPost, function(response) {
         var verbRes = response.word;
-        $("#verb").text(verbRes);
+        $('#verb').text(verbRes);
       });
+
       verbLocked = true;
     }
 
-    if(noun) {
+    if (noun) {
       nounPost = {word: noun};
-      $.post("/locked-noun", nounPost, function(response){
+      $.post('/locked-noun', nounPost, function(response) {
         var nounRes = response.word;
-        $("#noun").text(nounRes);
+        $('#noun').text(nounRes);
       });
+
       nounLocked = true;
     }
   });
 
-  $("#unlock").on('click', function(e){
+  //manual unlock button
+  $('#unlock').on('click', function(e) {
     e.preventDefault();
 
     adjLocked = false;
@@ -77,42 +89,41 @@ $(function() {
     nounLocked = false;
   });
 
-  $("#submitWords").on('submit', function(e){
+  //submit words feature
+  $('#submitWords').on('submit', function(e) {
     //prevents front page from reloading
     e.preventDefault();
 
-    var adjective = $("input[name=adjective]").val();
-    var verb = $("input[name=verb]").val();
-    var noun = $("input[name=noun]").val();
-    //this ifstatement checks to see if adjective is thruthy ie: is there a word in adjective
+    var adjective = $('input[name=adjective]').val();
+    var verb = $('input[name=verb]').val();
+    var noun = $('input[name=noun]').val();
+
     var adjPost;
     var verbPost;
     var nounPost;
 
     if (adjective) {
       adjPost = {word: adjective};
-      $.post("adjective", adjPost, function(response){
+      $.post('adjective', adjPost, function(response) {
         var adjectiveRes = response.msg;
-        $("#adjectiveRes").text(adjectiveRes);
+        $('#adjectiveRes').text(adjectiveRes);
       });
     }
 
     if (verb) {
       verbPost = {word: verb};
-      $.post("verb", verbPost, function(response){
+      $.post('verb', verbPost, function(response) {
         var verbRes = response.msg;
-        $("#verbRes").text(verbRes);
+        $('#verbRes').text(verbRes);
       });
     }
 
     if (noun) {
       nounPost = {word: noun};
-      $.post("noun", nounPost, function(response){
+      $.post('noun', nounPost, function(response) {
         var nounRes = response.msg;
-        $("#nounRes").text(nounRes);
+        $('#nounRes').text(nounRes);
       });
     }
   });
-
-
 });
--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 
 //grabs the express object and assigns it to a variable. doing this will allow me to use the express object
 var express = require('express');
-var bodyparser = require("body-parser");
+var bodyparser = require('body-parser');
 
-var Adjective = require("./lib/adjective.js");
-var Verb = require("./lib/verb.js");
-var Noun = require("./lib/noun.js");
-var getRandomWord = require("./lib/getRandomWord.js");
-var postRandomWord = require("./lib/postRandomWord.js");
-//var resRandomWord = require("./lib/resRandomWord.js");
+var Adjective = require('./lib/adjective.js');
+var Verb = require('./lib/verb.js');
+var Noun = require('./lib/noun.js');
+var getRandomWord = require('./lib/getRandomWord.js');
+var postRandomWord = require('./lib/postRandomWord.js');
 
 var app = express();
+
 //creates a door with the key. This means if there is no PORT environmental variable, then use 3000
 var port = process.env.PORT || 3000;
 
@@ -21,13 +21,12 @@ app.use(bodyparser.urlencoded({extended: true}));
 //server will make app file available
 app.use(express.static(__dirname + '/app/'));
 
-var adjective = new Adjective;
-var verb = new Verb;
-var noun = new Noun;
-
+var adjective = new Adjective();
+var verb = new Verb();
+var noun = new Noun();
 
 //.send is a express method. this gets the / which represents  the home page ie: www.google.com/ this sends a RESponse
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
@@ -46,31 +45,32 @@ app.post('/adjective', postResponse(adjective));
 app.post('/verb', postResponse(verb));
 app.post('/noun', postResponse(noun));
 
-app.post('/locked-adj', function(req, res){
- var word = req.body
- res.json(word);
-});
-app.post('/locked-verb', function(req, res){
- var word = req.body
- res.json(word);
-});
-app.post('/locked-noun', function(req, res){
- var word = req.body
- res.json(word);
+app.post('/locked-adj', function(req, res) {
+  var word = req.body;
+  res.json(word);
 });
 
+app.post('/locked-verb', function(req, res) {
+  var word = req.body;
+  res.json(word);
+});
+
+app.post('/locked-noun', function(req, res) {
+  var word = req.body;
+  res.json(word);
+});
 
 function getResponse(wordType) {
   return function(req, res) {
     res.json(getRandomWord(wordType));
-  }
+  };
 }
 
 function postResponse(wordType) {
   return function(req, res) {
     var word = postRandomWord(req.body.word, wordType);
     res.json(word);
-  }
+  };
 }
 
 //the express module has a listen function. this example takes in a port and a callback that does something with with port.
